588197597e3301eec796a76f7ac213366e6d45b6
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2023-08-16 21:48:06 +0530

    login ui component made

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from "@/components/LoginCard"
+
+export default function Login() {
+  return (
+    <main style={{display: 'flex', justifyContent: 'center', marginTop:'48px'}}>
+     <LoginCard />
+    </main>
+  )
+}
---
+++ components/LoginCard.tsx
@@ -0,0 +1,49 @@
+import * as React from "react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+export default function LoginCard() {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Welcome to pybeam</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Username</Label>
+              <Input id="name" placeholder="Enter your username" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Password</Label>
+              <Input id="name" placeholder="Enter your password" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button style={{width : '50%'}}>Login</Button>
+        <Button variant="outline">Forgot</Button>
+        
+      </CardFooter>
+    </Card>
+  );
+}
