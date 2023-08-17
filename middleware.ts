3307675b98180cbe1324f7cf7620e74c8091fbba
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2023-08-17 13:03:00 +0530

    basic auth setup with static value
    based on middleware

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,20 @@
 import { NextResponse } from "next/server";
 /**
- * 
- * @param req 
- * @returns 
+ *
+ * @param req
+ * @returns
  */
-export default function middleware(req: Request) {
-   
-    const time = Date.now()
-    const timeStr = new Date(time).toLocaleDateString();
 
-    const logData = {
-        time: timeStr,
-        url: req.url
-    }
-    // console.log('logData', logData)
-   return NextResponse.next();
-   
-}
+export { default } from "next-auth/middleware";
+
+// export function middleware(req: Request) {
+//   const time = Date.now();
+//   const timeStr = new Date(time).toLocaleDateString();
+
+//   const logData = {
+//     time: timeStr,
+//     url: req.url,
+//   };
+//   // console.log('logData', logData)
+//   return NextResponse.next();
+// }
