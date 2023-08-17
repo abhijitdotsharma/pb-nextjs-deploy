0363313d9069a504fbac43c0ec6be24d8d8df306
abhijitdotsharma <sharma.abhijit2050@gmail.com>
2023-08-17 09:45:25 +0530

    add api route request type

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,7 +1,8 @@
 import connectDB from "@/lib/connect-db";
+import { NextApiRequest } from "next/types";
 
 connectDB();
 
-export async function GET(Request) {
+export async function GET(Request: NextApiRequest) {
     return new Response("This is a new API route");
 }
